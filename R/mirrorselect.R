a99626e5e0b60f3772fec5e3f32b50dab6c1596a
Guangchuang Yu <guangchuangyu@gmail.com>
2019-05-23 16:25:00 +0800

    test bioc speed

--- R/mirrorselect.R
+++ R/mirrorselect.R
@@ -1,6 +1,12 @@
 mirrorselect_internal <- function(mirrors) {
+    if (any(grepl('bioc', mirrors))) {
+        file <- "packages/release/bioc/html/ggtree.html"
+    } else {
+        file <- "src/base/COPYING"
+    }
+
     res <- vapply(mirrors, function(url) {
-        url <- paste0(url, "src/base/COPYING")
+        url <- paste0(url, file)
         tryCatch(system.time(downloader(url))[["elapsed"]],
                  error = function(e) NA)
     }, FUN.VALUE = numeric(1))
@@ -15,14 +21,34 @@ downloader <- function(url) {
 ##'
 ##' 
 ##' @title get_mirror
+##' @param repo one of 'CRAN' or 'BioC'
 ##' @return a vector of mirror urls
 ##' @export
+##' @examples
+##' head(get_mirror())
 ##' @author Guangchuang Yu
-get_mirror <- function() {
+get_mirror <- function(repo = "CRAN") {
+    if  (repo == "CRAN") {
+        return(get_mirror_cran())
+    }
+
+    get_mirror_bioc()
+}
+
+get_mirror_cran <- function() {
     url <- "https://cran.r-project.org/mirrors.html"
     x <- readLines(url)
-    mirrors <- unique(sub(".*(https{0,1}://[a-zA-z\\.\\/]+).*", "\\1", x[grep("^<a", x)]))
-    return(mirrors)
+    unique(sub(".*(https{0,1}://[a-zA-z\\.\\/]+).*", "\\1", x[grep("^<a", x)]))
+}
+
+##' @importFrom magrittr %>%
+get_mirror_bioc <- function() {
+    url <- 'https://www.bioconductor.org/about/mirrors/'
+    x <- readLines(url)
+    x[grep('URLs', x)] %>% strsplit(';') %>%
+        unlist %>% gsub('<[^>]+>', '', .) %>%
+        sub("URLs:", '', .) %>%
+        sub("\\s+", '', .)
 }
 
 ##' test download speed of CRAN mirrors
@@ -33,6 +59,11 @@ get_mirror <- function() {
 ##' @param mirrors a vector of CRAN mirrors
 ##' @return data frame with a column of mirror and second column of speed
 ##' @export
+##' @examples
+##' x <- mirrorselect(c("http://cran.stat.upd.edu.ph/",
+##'                   "http://healthstat.snu.ac.kr/CRAN/",
+##'                   "http://cran.ism.ac.jp/"))
+##' head(x)
 ##' @author Guangchuang Yu
 mirrorselect <- function(mirrors) {
     speed <- mirrorselect_internal(mirrors)
@@ -43,3 +74,4 @@ mirrorselect <- function(mirrors) {
 }
 
 
+utils::globalVariables(".")
